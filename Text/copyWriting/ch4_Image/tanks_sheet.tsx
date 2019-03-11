fc6df39cad4af102488a17c00d6b8b72cffe30a2
Kaede <curiosity.yg57@gmail.com>
2019-03-11 16:34:16 +0900

    update

---
+++ Text/copyWriting/ch4_Image/tanks_sheet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tanks_sheet" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../ch4/tanks_sheet.png" width="256" height="128"/>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="14" y="7" width="14" height="22">
+    <ellipse/>
+   </object>
+   <object id="2" x="165" y="-1">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
