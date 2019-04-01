0f57226b4cebf00a9bbdb2b33e4c127568cf01d5
Kaede <curiosity.yg57@gmail.com>
2019-04-01 17:14:32 +0900

    add blur

--- copyWriting/core01/sun.js
+++ copyWriting/core01/sun.js
@@ -5,12 +5,13 @@ const render = () => {
     tick++; // This val only increase
     ctx.clearRect(0,0,endX,endY);
 
-    ctx.shadowblur = 10000;
     ctx.shadowColor = '#FFF';
     ctx.beginPath();
     ctx.fillStyle = 'yellow';
+    ctx.shadowblur = 30;
     ctx.arc(sun.x, sun.y, sun.r, 0,2*Math.PI,false);
     ctx.fill();
+    ctx.shadowblur = 0;
     //dot(sun);
     satelite(sun,earth);
     satelite(earth,moon);
